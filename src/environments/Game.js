b6a10db02f0a5d4c03cd397ddf6aacb17e2eeeda
Hamid Zare <dellydela@gmail.com>
2018-11-07 18:29:19 -0600

    converted Game model implementation to JS

---
+++ src/environments/Game.js
@@ -0,0 +1,34 @@
+// game super class
+
+class Game {
+  constructor(opts) {
+    console.log('creating game driver');
+    this._test();
+  }
+
+  get state() {
+    throw new Error('Method is not implemented.');
+  }
+
+  async step(action) {
+    throw new Error('Method is not implemented.');
+  }
+
+  reset() {
+    throw new Error('Method is not implemented.');
+  }
+
+  async act(action) {
+    throw new Error('Method is not implemented.');
+  }
+
+  // run once after an action to get its feedback
+  get feedback() {
+    throw new Error('Method is not implemented.');
+  }
+
+  // basic tests for the implemented game
+  _test() {
+    console.assert(this.state() instanceof tf.Tensor || Array.isArray(this.state()), 'state is not formatted properly');
+  }
+}
