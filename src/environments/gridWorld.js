aed72f8737e5cdbbb54998b9c82093e171720bc0
Hamid Zare <dellydela@gmail.com>
2018-11-09 13:19:38 -0600

    == to === and column to col

--- src/environments/gridWorld.js
+++ src/environments/gridWorld.js
@@ -1,49 +1,49 @@
 const assert = require('assert'),
   Game = require('./Game.js');
 
-// TODO rename columns to cols
+// TODO rename cols to cols
 
 const TERMINAL_SYMBOL='[T]';
 const PLAYER_SYMBOL='[P]';
 const EMPTY_SYMBOL='[ ]';
 
 class GridWorld extends Game {
-  constructor(rows=4, columns=4) {
+  constructor(rows=4, cols=4) {
     super();
 
     this.grid = Array(rows).fill(null).map(() => {
-      return Array(columns).fill(EMPTY_SYMBOL);
+      return Array(cols).fill(EMPTY_SYMBOL);
     });
 
     this.rows = rows;
-    this.columns = columns;
+    this.cols = cols;
 
     this.reset();
   }
 
-  _markCellTerminal(row, column) {
-    this.grid[row][column] = TERMINAL_SYMBOL;
+  _markCellTerminal(row, col) {
+    this.grid[row][col] = TERMINAL_SYMBOL;
     // TODO no tuples in js, add list instead to the terminalCells?
     for (let i = 0; i < this.terminalCells.length; ++i) {
-      if (this.terminalCells[i][0] === row && this.terminalCells[i][1] === column) {
-        // Maintain uniqueness of (row,column) in terminalCells
+      if (this.terminalCells[i][0] === row && this.terminalCells[i][1] === col) {
+        // Maintain uniqueness of (row,col) in terminalCells
         return;
       }
-      this.terminalCells.push([row, column]);
+      this.terminalCells.push([row, col]);
     }
   }
 
-  _setPlayerAt(row, column) {
+  _setPlayerAt(row, col) {
     // TODO validation checks?
-    if (row > this.rows-1 || column > this.columns-1 || row < 0 || column < 0) {
-      throw new Error(`invalid location ${row} ${column}`);
+    if (row > this.rows-1 || col > this.cols-1 || row < 0 || col < 0) {
+      throw new Error(`invalid location ${row} ${col}`);
     }
-    this._pLoc = [row, column];
+    this._pLoc = [row, col];
   }
 
   _isOver() {
     for (let i = 0; i < this.terminalCells.length; ++i) {
-      if (this._pLoc[0] == this.terminalCells[i][0] && this._pLoc[1] == this.terminalCells[i][1]) {
+      if (this._pLoc[0] === this.terminalCells[i][0] && this._pLoc[1] === this.terminalCells[i][1]) {
         return true;
       }
     }
@@ -54,7 +54,7 @@ class GridWorld extends Game {
     let row = this._pLoc[0];
     let col = this._pLoc[1];
     //TODO can add rowColToIndex util function if desired
-    return row * this.columns + col;
+    return row * this.cols + col;
   }
 
   show() {
@@ -62,7 +62,7 @@ class GridWorld extends Game {
       let disp_line = '';
       for (let j = 0; j < this.grid[i].length; ++j) {
         let symbol = this.grid[i][j];
-        if (i == this._pLoc[0] && j == this._pLoc[1]) {
+        if (i === this._pLoc[0] && j === this._pLoc[1]) {
           symbol = PLAYER_SYMBOL;
         }
         disp_line += symbol;
@@ -76,16 +76,16 @@ class GridWorld extends Game {
     // TODO syntax check
 
     const rows = this.rows;
-    const columns = this.columns;
+    const cols = this.cols;
 
     this.terminalCells = [];
 
     // put player at init location
-    this._pLoc = [rows-1, columns-1];
+    this._pLoc = [rows-1, cols-1];
     this._setPlayerAt(rows-1, 0);
 
     this._markCellTerminal(0, 0);
-    this._markCellTerminal(rows-1, columns-1);
+    this._markCellTerminal(rows-1, cols-1);
   }
 
   // 0, 1, 2, 3 = up, down, right, left
@@ -96,10 +96,10 @@ class GridWorld extends Game {
 
     // CHECK
     try {
-      if (action == 0) this._setPlayerAt(y-1, x);
-      if (action == 1) this._setPlayerAt(y+1, x);
-      if (action == 2) this._setPlayerAt(y, x+1);
-      if (action == 3) this._setPlayerAt(y, x-1);
+      if (action === 0) this._setPlayerAt(y-1, x);
+      if (action === 1) this._setPlayerAt(y+1, x);
+      if (action === 2) this._setPlayerAt(y, x+1);
+      if (action === 3) this._setPlayerAt(y, x-1);
     } catch (e) {
       // stay in the same cell
     }
@@ -118,11 +118,11 @@ class GridWorld extends Game {
 
 
 g = new GridWorld();
-assert(g.state() == 12);
+assert(g.state() === 12);
 g.act(0);
-assert(g.state() == 8);
+assert(g.state() === 8);
 g.act(2);
-assert(g.state() == 9);
+assert(g.state() === 9);
 g.show();
 
 
